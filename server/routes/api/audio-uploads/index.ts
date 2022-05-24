2d48072617bc33c77e3c078863218b292f1fd461
Edwin Shen <edshen17@avenues.org>
2022-05-24 18:14:29 -0400

    finish getAudioUpload route

--- server/routes/api/audio-uploads/index.ts
+++ server/routes/api/audio-uploads/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { makeCreateAudioUploadController } from '../../../components/controllers/audio-upload/create-audio-upload-controller';
+import { makeGetAudioUploadController } from '../../../components/controllers/audio-upload/get-audio-upload-controller';
 import { makeJsonExpressCallback } from '../../../components/web-driver-callbacks/express';
 
 const audioUploads = express.Router();
 
 audioUploads.post('/', makeJsonExpressCallback.consume(makeCreateAudioUploadController));
-// audioUploads.get('/:audioUploadId', makeJsonExpressCallback.consume(makeGetAudioUploadUseCase))
+audioUploads.get('/:audioUploadId', makeJsonExpressCallback.consume(makeGetAudioUploadController));
 
 export { audioUploads };
